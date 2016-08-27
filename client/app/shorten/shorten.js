4ecf904552e0773fbd7d8a84146c6953e19d6d23
oldchevy <nick.olszowy@gmail.com>
2016-08-26 20:02:22 -0700

    Refactored to live form validation

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -6,15 +6,14 @@ angular.module('shortly.shorten', [])
     url: $scope.newLink
   };
 
-  $scope.addLink = function() {
+  $scope.checkUrl = function(url) {
     var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
-    if (!!$scope.newLink.match(rValidUrl)) {
-      Links.addOne(JSON.stringify({url: $scope.newLink}));
-      $scope.errMessage = '';
-    } else {
-      $scope.errMessage = 'Error: Not a valid url';
-    }
-    $scope.newLink = '';
+    return (url && url.match(rValidUrl) ? '' : 'Invalid URL');
+  };
+
+  $scope.addLink = function() {
+    Links.addOne(JSON.stringify({url: $scope.newLink}));
+    $scope.errMessage = '';
   };
 
 
